6a4cedb5f913df711d5148836d7c4c18ac4dc790
dukler <iarwain@hotmail.com.ar>
2019-10-08 03:33:59 -0300

    nothing

--- migrations/7_add_dev_test.up.sql
+++ migrations/7_add_dev_test.up.sql
@@ -17,6 +17,7 @@ DO $$
         select id into userID from users where username = 'dukler';
         select id into mockAppID from app where name = 'mockApp';
         insert into domains (name,app_id) values ('localhost:3000',mockAppID);
+        insert into domains (name,app_id) values ('duckstack.com',mockAppID);
         select id into loginAppID from app where name = 'login';
         select id into domainID from domains where name = 'localhost:3000';
         INSERT INTO users_app (user_id,app_id) VALUES (userID, mockAppID);
